5394735f79c609b3973db16a9c142c5965735223
lildev1-admin <skap0329@naver.com>
2023-05-22 17:27:07 +0900

    Feat: 아이템 사용 버튼 추가

---
+++ Assets/Script/Tutorial Scripts/JumpBtn.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38969a461272dad783d249a3c03bb1067376be75bc51ca1018b47112d7dab4a7
+size 454
---
+++ Assets/Script/Tutorial Scripts/UseItemBtn.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a280ecf9d6b2c55edd67358cbfb460ffddedb2b690f68f89db94969d85346354
+size 477
