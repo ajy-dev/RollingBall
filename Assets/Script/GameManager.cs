f302a7dd9961a5fe20b99721a6f70e18dee7867f
lildev1-admin <skap0329@naver.com>
2023-05-28 03:01:58 +0900

    코인 기능 구현 완료, 튜토리얼 Bridge에서 점프가 안되는 현상 해결

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29c7e12d0ece59545fb6c45aa1ecb6c0a36dd3d510b21d3b9ed338288b9edcf1
+size 513
---
+++ Assets/Script/Tutorial Scripts/Coin.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0c255c53dad48743443abb9ba9dddb5cf81007c9c08069f711a17cb4afe456b
+size 508
